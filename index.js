dfc93797c237346997097ed8b649c83a124d7e42
Jennifer Imafion <lta23@txstate.edu>
2025-09-01 18:07:20 -0500

    added game cards

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 // import the JSON data about the crowd funded games from the games.js file
 import GAMES_DATA from './games.js';
-
+console.log("index.js loaded");
 // create a list of objects to store the data about the games using JSON.parse
 const GAMES_JSON = JSON.parse(GAMES_DATA)
 
@@ -29,27 +29,33 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+    for (let i = 0; i < games.length; i++) {
+        const game = games[i];
         // create a new div element, which will become the game card
-
+        let new_div = document.createElement('div');
 
         // add the class game-card to the list
-
+        new_div.classList.add('game-card');
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
+        const display = `
+            <img src="${game.img}" class="game-img" />
+            <h3>${game.name}</h3>
+            <p>${game.description}</p>
+        `;
 
-
+        new_div.innerHTML = display;
         // append the game to the games-container
-
+        gamesContainer.appendChild(new_div);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+console.log(addGamesToPage(GAMES_JSON));
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
