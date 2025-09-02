812ec1e3c1a4c5750974231d97a5109bb899a683
Jennifer Imafion <lta23@txstate.edu>
2025-09-01 19:40:58 -0500

    Display total count for contribution / raised / game cards

--- index.js
+++ index.js
@@ -67,20 +67,28 @@ console.log(addGamesToPage(GAMES_JSON));
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( (acc, contributions) => {
+    return acc + contributions.backers
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = totalContributions;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
+const totalRaisedCard = GAMES_JSON.reduce( (acc, pledge) => {
+    return acc + pledge.pledged
+}, 0);
 // set inner HTML using template literal
-
+raisedCard.innerHTML = "$" + totalRaisedCard;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+//const totalGames = GAMES_JSON.length;
+const totalGames = GAMES_JSON.length;
+gamesCard.innerHTML = totalGames;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
