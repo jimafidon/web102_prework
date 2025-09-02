79eb8c55940861c7a056d3b04d392f429dffe7a0
Jennifer Imafion <lta23@txstate.edu>
2025-09-01 22:04:18 -0500

    Added the commas to the numbers in the total cards

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const totalContributions = GAMES_JSON.reduce( (acc, contributions) => {
 }, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-contributionsCard.innerHTML = totalContributions;
+contributionsCard.innerHTML = totalContributions.toLocaleString();
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
@@ -81,7 +81,7 @@ const totalRaisedCard = GAMES_JSON.reduce( (acc, pledge) => {
     return acc + pledge.pledged
 }, 0);
 // set inner HTML using template literal
-raisedCard.innerHTML = "$" + totalRaisedCard;
+raisedCard.innerHTML = "$" + totalRaisedCard.toLocaleString();
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
